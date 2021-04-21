f34646dc9aab0d186603467d93d67dc29732a24a
Branc0 <rrochadeazevedo@gmail.com>
2021-04-20 22:34:59 -0300

    criando meu primeiro post e get

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,12 +1,18 @@
 const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('voce esá na rota de atendimento e esá realizando um GET'));
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res);
+    });
 
-    app.get('', (req, res) => res.send('acesso a rota Inicial'));
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        Atendimento.busca(id, res);
+    });
 
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body;
-        Atendimento.adiciona(atendimento);
+        // enviamos nossa instância de res para dentro da função assim conseguimos determinar um valor como resposta lá dentro
+        Atendimento.adiciona(atendimento, res);
     })
 }
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,18 +1,65 @@
-const moment = require('moment')
-const conexao = require('../infra/conexao')
+const moment = require('moment');
+const conexao = require('../infra/conexao');
 
 // responsabilidade de realizar as querys no banco
 class Atendimento {
-    adiciona(atendimento) {
-        // const dataCriacao = new Date();
-        // const data = moment()
+    adiciona(atendimento, res) {
+        // cria em uma variável a data atual do momento de criação do registro no BD
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
+        // Pega a data submetida pelo usuário e converte ela
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+        // uso o spread operator para copiar todos os dados do objeto original e envio novos atributos
+        atendimento = { ...atendimento, data, dataCriacao };
+
+
+        // Validações
+        const isValidDateExpression = moment(data).isSameOrAfter(dataCriacao);
+
+        // const validacoes = [ 
+        //  {
+        //      name: 'data',
+        //      valido: validDateExpression,
+        //      mensagem: 'Data deve ser maior ou igual a data atual'
+        //  }
+        // ]
+
+        // const errors = validacoes.filter(campo => !campo.valido)
+
+        if (!isValidDateExpression) {
+            res.status(400).json('Data deve ser maior ou igual a data atual');
+        }
         // o ponto de interrogação no final da query, tem o papel de ser wildcard, vai representar o que iremos passar de valor no próximo parâmetro
-        const sql = `INSERT INTO Atendimentos SET ?`
+        const sql = `INSERT INTO Atendimentos SET ?`;
         conexao.query(sql, atendimento, (erro, resultados) => {
             if (erro) {
-                console.log(erro);
+                // retornamos como resposta o erro em forma de JSON e adicionamos tambem o status
+                res.status(400).json(erro);
             } else {
-                console.log(resultados);
+                // retornamos como resposta o resultado em forma de JSON
+                res.status(201).json(resultados);
+            }
+        })
+    }
+
+    lista(res) {
+        const query = 'SELECT * FROM Atendimentos'
+        conexao.query(query, (erro, resultados) => {
+            if (erro) {
+                res.status(500).json(erro);
+            } else {
+                res.status(200).json(resultados);
+            }
+        })
+    }
+
+    busca(id, res) {
+        const query = `SELECT * FROM Atendimentos WHERE id=${id}`
+        conexao.query(query, (erro, resultados) => {
+            if (erro) {
+                res.status(500).json(erro);
+            } else {
+                resultados = resultados[0];
+                res.status(200).json(resultados);
             }
         })
     }
